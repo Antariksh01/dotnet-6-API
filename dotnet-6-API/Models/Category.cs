afb9f020d60e0c71b5bb0f58ee50f40e7a2a9ff7
Antariksha Sharma <Antariksha_Sharma@epam.com>
2022-08-17 20:11:59 +0530

    Models folder added with Character class and Category enum.

---
+++ dotnet-6-API/Models/Category.cs
@@ -0,0 +1,9 @@
+﻿namespace dotnet_6_API.Models
+{
+    public enum Category
+    {
+        God = 0,
+        King = 1,
+        Warrior = 2
+    }
+}
---
+++ dotnet-6-API/Models/Character.cs
@@ -0,0 +1,14 @@
+﻿namespace dotnet_6_API.Models
+{
+    public class Character
+    {
+        public int Id { get; set; } = 0;
+        public string Name { get; set; } = "Krishna";
+        public int Strength { get; set; } = 10;
+        public int Intelligence { get; set; } = 10;
+        public int Stamina { get; set; } = 10;
+        public int HitPoints { get; set; } = 10;
+        public Category Category { get; set; } = Category.God;
+
+    }
+}
